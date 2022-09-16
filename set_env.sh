5fdb26731c1a2e42f53857a6c9981b4a7e5d18a9
Ribiro <deno059861>
2022-09-16 20:46:05 +0300

    first commit

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=ribiro
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=ribiro-project3-database.cfa7ulwuxact.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=ribiro-project3-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=project3
 export URL=http://localhost:8100
