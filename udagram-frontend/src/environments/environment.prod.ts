34686ac5b157b4b46063dbd08e83cbe7920e25af
Ribiro <deno059861>
2022-09-17 07:02:17 +0300

    finalizing

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ad1dea7f7f1564d929d0097bcb3c7434-2054425214.us-east-1.elb.amazonaws.com'
 };
 
 /*
