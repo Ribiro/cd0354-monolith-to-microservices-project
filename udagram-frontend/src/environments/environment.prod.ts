75b360d0c38c83f17d0747fbcd5ff8cc0864ebc2
Ribiro <deno059861>
2022-09-17 14:44:23 +0300

    revision

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'ad1dea7f7f1564d929d0097bcb3c7434-2054425214.us-east-1.elb.amazonaws.com'
+  apiHost: 'ad1dea7f7f1564d929d0097bcb3c7434-2054425214.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
